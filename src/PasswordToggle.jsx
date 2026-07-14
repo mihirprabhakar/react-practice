81c6bba8609cfeb51c42e70c372b35604a8cec1e
mihirprabhakar <mihirprabhakar6@gmail.com>
2026-07-14 23:47:41 +0530

    removed the arrow function

--- src/PasswordToggle.jsx
+++ src/PasswordToggle.jsx
@@ -2,13 +2,13 @@ import  { useState } from 'react'
 
 const PasswordToggle = () => {
     const [showPassword,setShowPassword]=useState(false);
-    const showPasswordButton=()=>{
-        setShowPassword(!showPassword);
-    }
+    // const showPasswordButton=()=>{
+    //     setShowPassword(!showPassword);
+    // }
   return (
     <div>
       <input type={showPassword?"text":"password"} placeholder='enter the password'></input>
-      <button onClick={showPasswordButton}>{showPassword?"Hide":"Show"}</button>
+      <button onClick={()=>setShowPassword(!showPassword)}>{showPassword?"Hide":"Show"}</button>
     </div>
   )
 }
